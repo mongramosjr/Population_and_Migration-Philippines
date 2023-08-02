a542b1285b05b299df5850e841b989dd66fba033
Mong Ramos Jr <mongramosjr@gmail.com>
2023-08-02 21:17:38 +0800

    added Philippine Standard Geographic Code, changed from Administrative Subdivision to geographic_level

--- utils/utils.py
+++ utils/utils.py
@@ -2,20 +2,20 @@ import pandas as pd
 
 def transform_csv_to_parquet(filename, folder):
     df = pd.read_csv(folder + filename + '.csv')
-  
-    df['Territorial Subdivision'].mask(df['Geographic Location']=="PHILIPPINES", 
+    df['geographic_level'] = pd.Series(["" for x in range(len(df.index))])
+    df['geographic_level'].mask(df['Geographic Location']=="PHILIPPINES", 
                                        "Country", inplace=True)
-    df['Territorial Subdivision'].mask(df['Geographic Location'].str.contains("^\.{4}"), 
+    df['geographic_level'].mask(df['Geographic Location'].str.contains("^\.{4}"), 
                                        "Province", inplace=True)
-    df['Territorial Subdivision'].mask((df['Geographic Location'].str.contains("City") 
+    df['geographic_level'].mask((df['Geographic Location'].str.contains("City") 
                                        & ~df['Geographic Location'].str.contains("excluding")), 
                                        "City", inplace=True)
-    df['Territorial Subdivision'].mask(df['Geographic Location'].str.contains("^\.{4}Pateros"), 
+    df['geographic_level'].mask(df['Geographic Location'].str.contains("^\.{4}Pateros"), 
                                        "Municipality", inplace=True)
     
-    df['Territorial Subdivision'].mask(df['Geographic Location'].str.contains("^\.{2}[a-z A-Z]+"), 
+    df['geographic_level'].mask(df['Geographic Location'].str.contains("^\.{2}[a-z A-Z]+"), 
                                        "Region", inplace=True)
-    df['Territorial Subdivision'].mask(df['Geographic Location'].str.contains("Region"), 
+    df['geographic_level'].mask(df['Geographic Location'].str.contains("Region"), 
                                        "Region", inplace=True)
     
     df['Geographic Location'] = df['Geographic Location'].str.lstrip(".")
